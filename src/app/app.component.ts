35088028e37d2f2a4e0c55d1dd1bc46190617cf9
Muretivin <muretivincent6@gmail.com>
2025-01-22 15:14:55 +0300

    attribute directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,13 @@ import { RouterOutlet } from '@angular/router';
 import { DataBindingComponent } from './components/data-binding/data-binding.component';
 import { NgIfComponent } from './components/ng-if/ng-if.component';
 import { NgForComponent } from './components/ng-for/ng-for.component';
+import { NgClassComponent } from './components/ng-class/ng-class.component';
+import { FormsModule } from '@angular/forms';
+import { NgStyleComponent } from './components/ng-style/ng-style.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NgForComponent],
+  imports: [RouterOutlet, NgClassComponent, FormsModule, NgStyleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
